cb331384504b2c88149b5716ea642241912d8e33
Michael <mejevin@gmail.com>
2022-05-14 06:15:39 +0300

    [Add] No DI container example

---
+++ Examples/ConsoleAppNoGenericHostExample/CryptlexClientFactory.cs
@@ -0,0 +1,33 @@
+﻿using Cryptlex.Net.Core;
+using Cryptlex.Net.Core.Services;
+using Microsoft.Extensions.Options;
+
+public class MyHttpClientFactory : IHttpClientFactory
+{
+    public HttpClient CreateClient(string name)
+    {
+        var client = new HttpClient();
+        return client;
+    }
+}
+
+public static class CryptlexClientFactory
+{
+    public static ICryptlexClient Create()
+    {
+        var token = "YOUR TOKEN";
+
+        var cryptlexClientSettings = new CryptlexClientSettings() { AccessToken = token };
+
+        var httpClientFactory = new MyHttpClientFactory();
+        var options = Options.Create(cryptlexClientSettings);
+
+        var licenses = new LicensesService(httpClientFactory, options);
+        var activations = new ActivationsService(httpClientFactory, options);
+        var tags = new TagsService(httpClientFactory, options);
+
+        var client = new CryptlexClient(licenses, activations, tags);
+
+        return client;
+    }
+}
---
+++ Examples/ConsoleAppNoGenericHostExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using Cryptlex.Net.Licenses;
+
+var _cryptlexClient = CryptlexClientFactory.Create();
+
+Console.WriteLine("Fetching licenses from cryptlex...\n");
+
+var licenses = await _cryptlexClient.Licenses.GetAllAsync(new GetAllLicensesData() { page = 1 });
+
+Console.WriteLine($"\nDone! Got {licenses.Count()} licenses from cryptlex: ");
+foreach (var license in licenses) Console.WriteLine(license.id);
+Console.WriteLine();
+
+Console.ReadKey();
