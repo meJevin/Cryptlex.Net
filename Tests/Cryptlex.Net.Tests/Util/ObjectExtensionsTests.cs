f82bb479efe6d3652d3a45a48d3ca16bed1fd9a4
Michael <mejevin@gmail.com>
2022-05-15 09:06:23 +0300

    [Add] Some unit tests

---
+++ Tests/Cryptlex.Net.Tests/Util/ObjectExtensionsTests.cs
@@ -0,0 +1,60 @@
+﻿using Cryptlex.Net.Util;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Cryptlex.Net.Tests.Util
+{
+    public class ObjectExtensionsTests
+    {
+        [Fact]
+        public void ToQueryString_Ignores_Null_Fields()
+        {
+            var obj = new { field1 = "value1", something = (string)null! };
+
+            var expected = "field1=value1";
+
+            var actual = obj.ToQueryString(true);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToQueryString_Preserves_Null_Fields()
+        {
+            var obj = new { field1 = "value1", something = (string)null! };
+
+            var expected = "field1=value1&something=null";
+
+            var actual = obj.ToQueryString(false);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToQueryString_Escapes_Strings()
+        {
+            var obj = new { field1 = "value1", something = "hello&" };
+
+            var expected = "field1=value1&something=hello%26";
+
+            var actual = obj.ToQueryString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToQueryString_Serializes_Arrays()
+        {
+            var obj = new { arr = new[] { 1, 2, 3, 4 } };
+
+            var expected = "arr=%5B1%2C2%2C3%2C4%5D";
+
+            var actual = obj.ToQueryString();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Tests/Cryptlex.Net.Tests/Util/UtilsTests.cs
@@ -0,0 +1,38 @@
+﻿using Cryptlex.Net.Util;
+using System;
+using Xunit;
+
+namespace Cryptlex.Net.Tests.Util
+{
+    public class UtilsTests
+    {
+        [Theory]
+        [InlineData("path1", "path2", "path3")]
+        [InlineData("path1", "path2")]
+        [InlineData("path1")]
+        public void CombinePaths_Produces_Correct_Results(params string[] strings)
+        {
+            // Arrange
+            var expected = string.Join("/", strings);
+
+            // Act
+            var actual = Utils.CombinePaths(strings);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("uri", "queryStr")]
+        [InlineData("uri", "")]
+        [InlineData("uri", null)]
+        public void AppendQueryString_Produces_Correct_Results(string uri, string queryStr)
+        {
+            var expected = uri + (string.IsNullOrEmpty(queryStr) ? "" : "?" + queryStr);
+
+            var actual = Utils.AppendQueryString(uri, queryStr);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
