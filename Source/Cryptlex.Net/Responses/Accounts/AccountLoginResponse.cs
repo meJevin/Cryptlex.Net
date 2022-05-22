802c8e0571a7b511823f1644558dcdf2e0ab5887
Michael <mejevin@gmail.com>
2022-05-22 04:59:11 +0300

    Typo!

--- Source/Cryptlex.Net/Responses/Accounts/AccountLoginResponse.cs
+++ Source/Cryptlex.Net/Responses/Accounts/AccountLoginResponse.cs
@@ -8,6 +8,6 @@ namespace Cryptlex.Net.Accounts
 {
     public class AccountLoginResponse
     {
-        string? accessToken { get; set; }
+        public string? accessToken { get; set; }
     }
 }
