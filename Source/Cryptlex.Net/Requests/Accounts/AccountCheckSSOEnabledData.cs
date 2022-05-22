8909e9f593d0f3db977d255ad894091a3b67c2d7
Michael <mejevin@gmail.com>
2022-05-22 03:18:37 +0300

    [Add] Missing methods for Account service
    closes #4

---
+++ Source/Cryptlex.Net/Requests/Accounts/AccountCheckSSOEnabledData.cs
@@ -0,0 +1,12 @@
+﻿namespace Cryptlex.Net.Accounts
+{
+    public class AccountCheckSSOEnabledData
+    {
+        public string comapnyId { get; set; }
+
+        public AccountCheckSSOEnabledData(string comapnyId)
+        {
+            this.comapnyId = comapnyId;
+        }
+    }
+}
---
+++ Source/Cryptlex.Net/Requests/Accounts/AccountInitiateSSOLoginData.cs
@@ -0,0 +1,12 @@
+﻿namespace Cryptlex.Net.Accounts
+{
+    public class AccountInitiateSSOLoginData
+    {
+        public string returnUrl { get; set; }
+
+        public AccountInitiateSSOLoginData(string returnUrl)
+        {
+            this.returnUrl = returnUrl;
+        }
+    }
+}
